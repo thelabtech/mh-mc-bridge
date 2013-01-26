55ae2812ce05d6613519b71504c77be5990d17a5
Baxter <blknaab@sbcglobal.net>
2013-01-26 12:11:55 -0800

    Unexpected G/D.....whatever

--- application/libraries/MailChimp.php
+++ application/libraries/MailChimp.php
@@ -1,8 +1,9 @@
 <?php
 
 class MailChimp {
-    private $lists = null;
-    private $api_key;
+    private $_lists = null;
+    private $_api_key;
+    private $_api;
     
     public function __construct($api_key) {
         $this->api_key = $api_key;
@@ -10,21 +11,27 @@ class MailChimp {
 
     public function build_lists()
     {
-        if ($this->lists === null)
+        if ($this->_lists === null)
         {
-            $api = new MCAPI($this->api_key);
-            $object = $api->lists();
+            $object = $this->api()->lists();
             $lists = $object['data'];
-            $this->lists = array();
+            $this->_lists = array();
             foreach ($lists as $list)
-                $this->lists[$list['id']] = $list['name'];
+                $this->_lists[$list['id']] = $list['name'];
         }
-        return $this->lists;
+        return $this->_lists;
     }
     
-    public function container_function()
+    public function add_contacts($list_id, $contacts)
     {
-        return $this->api_key;
+        $this->api()->listBatchSubscribe($list_id,$contacts);
+        return ;
     }
-       
+
+    public function api()
+    {
+        if ($this->_api === null)
+            $this->_api = new MCAPI($this->_api_key);
+        return $this->_api;
+    }
 }
