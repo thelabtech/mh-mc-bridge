df6c44a7b75a2c1746a450d9a6e5b3b3fa88ce61
trojancjs <007Zsnlp>
2013-01-26 12:20:30 -0800

    changed static "createTable" and "dropTable" functions to migrations.

---
+++ application/migrations/2013_01_01_000000_create_mappings_table.php
@@ -0,0 +1,25 @@
+<?php
+
+class Create_Mappings_Table {
+
+	public function up()
+	{
+        Schema::create('mappings', function($table) {
+            $table->increments('id');
+            $table->string('mission_hub_api_key');
+            $table->string('mission_hub_role');
+            $table->string('target_system');
+            $table->string('target_system_api_key');
+            $table->string('target_system_list');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->unique(array('mission_hub_api_key', 'mission_hub_role', 'target_system', 'target_system_api_key', 'target_system_list'));
+        });
+	}
+
+	public function down()
+	{
+		Schema::drop('mappings');
+	}
+
+}
--- application/models/Mapping.php
+++ application/models/Mapping.php
@@ -10,19 +10,4 @@ class Mapping extends Eloquent
     public $target_system;
     public $target_system_api_key;
     public $target_system_list;
-
-    public static function createTable() {
-
-        Schema::create('mappings', function($table) {
-            $table->increments('id');
-            $table->string('mission_hub_api_key');
-            $table->string('mission_hub_role');
-            $table->string('target_system');
-            $table->string('target_system_api_key');
-            $table->string('target_system_list');
-            $table->timestamp('created_at');
-            $table->timestamp('updated_at');
-            $table->unique(array('mission_hub_api_key', 'mission_hub_role', 'target_system', 'target_system_api_key', 'target_system_list'));
-        });
-    }
 }
