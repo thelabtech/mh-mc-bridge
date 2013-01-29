211b0fd6b48b7f919517a62d22e14df99e75b78b
Nate Emerson <emerson.nate@gmail.com>
2013-01-29 14:10:51 -0800

    Cleaned up output of cron

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -6,20 +6,22 @@ class Cron_Controller extends Base_Controller {
         $mappings = Mapping::all();
         foreach ($mappings as $mapping)
         {
-            echo 'mapping: '.$mapping->id.'<br>';
+            echo '<h1>Mapping: ID#'.$mapping->id.'</h1>';
             $mh = new MissionHub($mapping->mission_hub_api_key);
-            echo 'MissionHub: '.$mapping->mission_hub_api_key.'<br>';
+            echo '<p>MissionHub API Key: '.$mapping->mission_hub_api_key.'</p>';
             $mc = new MailChimp($mapping->target_system_api_key);
-            echo 'Mailchimp: '.$mapping->target_system_api_key.'<br>';
+            echo '<p>Mailchimp API Key: '.$mapping->target_system_api_key.'</p>';
             $list_id = $mapping->target_system_list;
-            echo 'list id: '.$list_id.'<br>';
+            echo '<p>Mailchimp list id: '.$list_id.'</p>';
             $role = $mapping->mission_hub_role;
-            echo 'mh_role: '.$role.'<br>';
+            echo '<p>Missionhub Role(Label?) ID: '.$role.'</p>>';
             $contacts = $mh->build_contacts($role);
+            echo '<h3>Contacts</h3><ul>';
             foreach ($contacts as $contact)
-                echo $contact['FNAME'].' '.$contact['LNAME'].(isset($contact['EMAIL']) ? ' ('.$contact['EMAIL'].')' : '').'<br>';
+                echo '<li>'.$contact['FNAME'].' '.$contact['LNAME'].(isset($contact['EMAIL']) ? ' ('.$contact['EMAIL'].')' : '(NO EMAIL').'</li>';
+            echo '</ul>';
             if (($error = $mc->add_contacts($list_id, $contacts)) != 1)
-                echo 'Error: '.$error;
+                echo '<h3 style="color: #FF0000;">Error: '.$error.'</h3>';
         }
     }
 }
